e9f1dc256611868e1ae5b4773901fa4c58976fc7
Ronard Luna <ronardevelops@gmail.com>
2020-12-16 06:57:49 -0500

    Authentication fully functional

---
+++ todo/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
+
+def home(request):
+    return render(request, 'todo/home.html')
+
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password = request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'A user with that username already exists'})
+        else:
+            return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'Passwords did not match'})
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), 'error': 'Username or password is incorrect'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect( 'home')
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
